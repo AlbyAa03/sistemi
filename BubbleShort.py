2bf7c3aa572475e69feeb5002ae7e3fbce380b55
AlbyAa03 <61047024+AlbyAa03@users.noreply.github.com>
2020-11-09 11:25:13 +0100

    Update BubbleShort.py

--- BubbleShort.py
+++ BubbleShort.py
@@ -1,22 +1,19 @@
 import random
-vet = [0]
+'''
+il mio programma....
+'''
 
-vet[0] = int ((random.random()*100)  / 1)
-for num in range(0,10):
-    vet.append(int (random.random()*100)  / 1)
-
-
-for numeri in vet:
-     print(numeri)
-
-
-def bubbleSort(vet, nums):
+def bubbleSort(vet):
+    nums=len(vet)
     for numeri in range(nums):
         for scambio in range(nums):
             if(vet[scambio]<vet[scambio+1]):
                 vet[scambio],vet[scambio+1] = vet[scambio+1],vet[scambio]
 
-bubbleSort(vet,10)
+vet=[] #lista vuota
+for num in range(0,10):
+    vet.append(random.randint(0,100))
+print(vet)
+bubbleSort(vet)
 print("-------------")
-for numeri in vet:
-    print(numeri)
+print(vet)
