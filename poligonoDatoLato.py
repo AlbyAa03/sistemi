705e29b2b3c8cf7bf3d8b85503f662d3309886db
AlbyAa03 <61047024+AlbyAa03@users.noreply.github.com>
2020-11-20 19:46:23 +0100

    Update poligonoDatoLato.py

--- poligonoDatoLato.py
+++ poligonoDatoLato.py
@@ -6,8 +6,6 @@ begin_fill()
 print("\n INSERIRE IL NUMERO DI Linserire numero dei lati")
 nLati = int(input())
 angolo = float(360 / nLati)
-
-
 for temp in range(0,nLati):
     right(angolo)
     forward(1000/nLati)
