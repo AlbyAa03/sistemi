3ed4e1c0bb8b278495ec3c6bcfe6870e06ef3a57
AlbyAa03 <61047024+AlbyAa03@users.noreply.github.com>
2020-11-20 19:45:50 +0100

    Add files via upload

---
+++ poligonoDatoLato.py
@@ -0,0 +1,16 @@
+from turtle import *
+goto(00,300)
+clear()
+color('red', 'yellow')
+begin_fill()
+print("\n INSERIRE IL NUMERO DI Linserire numero dei lati")
+nLati = int(input())
+angolo = float(360 / nLati)
+
+
+for temp in range(0,nLati):
+    right(angolo)
+    forward(1000/nLati)
+
+end_fill()
+done()
